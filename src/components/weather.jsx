e012a6510115fd6bc95efa61b6127de6f1bc7888
JiunJieLin <sl10066116@gmail.com>
2023-11-19 11:44:27 +0800

    delete z-index

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -8,14 +8,14 @@ const Weather = ({ selectedLocationIndex }) => {
 
   return (
     <>
-      <div className="flex justify-start flex-col text-white sm:justify-center sm:items-center relative">
-        <h1 className="text-[50px] sm:text-[35px] z-10 relative">
+      <div className="flex justify-start flex-col text-white sm:justify-center sm:items-center ">
+        <h1 className="text-[50px] sm:text-[35px]">
           {selectedLocation.location}
         </h1>
-        <p className="text-lg z-10 relative">{`${selectedLocation.day} ${selectedLocation.date}`}</p>
+        <p className="text-lg ">{`${selectedLocation.day} ${selectedLocation.date}`}</p>
       </div>
-      <div className="flex gap-10 sm:flex-col items-center">
-        <div className="flex pl-[100px] gap-2 sm:flex-col items-center sm:pl-0 ">
+      <div className="flex gap-10 sm:flex-col items-center ">
+        <div className="flex pl-[100px] gap-2 sm:flex-col sm:items-center sm: justify-cener sm:pl-0  ">
           <Image
             src={
               selectedLocation.weather === "Sunny"
@@ -25,15 +25,15 @@ const Weather = ({ selectedLocationIndex }) => {
                 : rainIcon
             }
             alt="cloud"
-            className="w-[180px] h-[180px] sm:opacity-40 sm:relative sm:z-0 sm:w-[150px] sm:h-[150px]"
+            className="w-[180px] h-[180px] sm:opacity-40  sm:w-[150px] sm:h-[150px] "
           />
-          <div className="text-white flex flex-col z-10 relative sm:items-center sm:justify-center ">
+          <div className="text-white flex flex-col  sm:items-center sm:justify-center ">
             <div className="text-[60px]">
-              {`${selectedLocation.hourlyTemperatures[0]}°`}
+              {`${selectedLocation.hourlyTemperatures[0]}°C`}
             </div>
             <p className="text-2xl">{selectedLocation.hourlyWeather[0]}</p>
           </div>
-          <div className="text-white flex flex-col gap-10 pl-8 sm:flex-row sm:pl-0  z-10 sm:relative sm:gap-3">
+          <div className="text-white flex flex-col pl-8  justify-around sm:flex-row sm:pl-0  sm:gap-3">
             <div className="flex flex-col items-center">
               <p>{selectedLocation.maxTemperature}</p>
               <p>Hight</p>
